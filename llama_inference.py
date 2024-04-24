ae1469f3cca8e8d91d401a1696408c32ba211f2f
Aryan Soman <aryan.soman10@gmail.com>
2024-04-24 16:14:49 -0400

    Greedy alg

--- llama_inference.py
+++ llama_inference.py
@@ -15,7 +15,7 @@ DTYPE = torch.float16
 passes = 0
 
 class LlamaAttentionHHOracle(LlamaSdpaAttention):
-    def __init__(self, regular_attn: LlamaSdpaAttention, heavy_hitters_prop=0.2, recent_prop=0.2, minimum_tokens=50):
+    def __init__(self, regular_attn: LlamaSdpaAttention, heavy_hitters_prop=0.2, recent_prop=0.2, minimum_tokens=None):
         super().__init__(regular_attn.config, regular_attn.layer_idx)
         self.q_proj = self.q_proj.to(DTYPE)
         self.k_proj = self.k_proj.to(DTYPE)
@@ -24,7 +24,7 @@ class LlamaAttentionHHOracle(LlamaSdpaAttention):
         self.training = False
         self.heavy_hitters_prop = heavy_hitters_prop
         self.recent_prop = recent_prop
-        self.minimum_tokens = minimum_tokens
+        self.minimum_tokens = 1 if minimum_tokens is None else minimum_tokens
 
     def forward(
         self,
@@ -82,27 +82,23 @@ class LlamaAttentionHHOracle(LlamaSdpaAttention):
 
         # upcast attention to fp32
         attn_scores = F.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-        attn_scores = F.dropout(attn_weights, p=self.attention_dropout, training=self.training)
+        attn_scores = F.dropout(attn_scores, p=self.attention_dropout, training=self.training)
         
         sum_scores = attn_scores.sum(axis=-2)
 
         num_keys = sum_scores.shape[-1]
 
         # create heavy hitter mask
-        num_hh = math.floor(num_keys * self.heavy_hitters_prop)
+        num_hh = max(self.minimum_tokens, math.floor(num_keys * self.heavy_hitters_prop))
         _, topk = sum_scores.topk(k=num_hh, dim=-1)
         is_heavy = torch.zeros_like(sum_scores, dtype=torch.bool) # (1, nh, nk)
         is_heavy = is_heavy.scatter(-1, topk, True).unsqueeze(2) # (1, nh, 1, nk)
         is_heavy = is_heavy.expand(is_heavy.shape[0], is_heavy.shape[1], attn_weights.shape[2], is_heavy.shape[3]) # (1, nh, nq, nk)
-        
-        # create recency mask
-        num_recent = max(self.minimum_tokens, math.floor(num_keys * self.recent_prop))
-        is_recent = torch.ones_like(attn_weights, dtype=torch.bool)
-        is_recent = torch.tril(is_recent, diagonal=num_recent)
-        is_recent = torch.triu(is_recent, diagonal=-num_recent)
 
         # create overall weights mask
-        keep_weights = torch.logical_or(is_heavy, is_recent)
+        keep_weights = is_heavy
+        if passes % 16 == 0:
+            print("Proportion of weights kept:", keep_weights.to(torch.float16).mean())
         
         # make the weights we don't want to keep small before we softmax
         attn_weights[~keep_weights] = torch.finfo(attn_weights.dtype).min
@@ -128,14 +124,14 @@ class LlamaAttentionHHOracle(LlamaSdpaAttention):
             attn_output = self.o_proj(attn_output)
 
         if not output_attentions:
-            attn_weights = None
+            attn_scores = None
         
         passes += 1
 
-        return attn_output, attn_weights, past_key_value
+        return attn_output, attn_scores, past_key_value
 
 class LlamaAttentionHHGreedy(LlamaSdpaAttention):
-    def __init__(self, regular_attn: LlamaSdpaAttention, heavy_hitters_prop=0.2, recent_prop=0.2, minimum_tokens=50):
+    def __init__(self, regular_attn: LlamaSdpaAttention, heavy_hitters_prop=0.2, recent_prop=0.2, minimum_tokens=None):
         super().__init__(regular_attn.config, regular_attn.layer_idx)
         self.q_proj = self.q_proj.to(DTYPE)
         self.k_proj = self.k_proj.to(DTYPE)
@@ -144,7 +140,10 @@ class LlamaAttentionHHGreedy(LlamaSdpaAttention):
         self.training = False
         self.heavy_hitters_prop = heavy_hitters_prop
         self.recent_prop = recent_prop
-        self.minimum_tokens = minimum_tokens
+        self.minimum_tokens = 1 if minimum_tokens is None else minimum_tokens
+        self.accum_sum_scores = None # (1, nh, nk)
+        self.evicted = None          # (1, nh, nk)
+        self.num_evicted = 0
 
     def forward(
         self,
@@ -199,48 +198,42 @@ class LlamaAttentionHHGreedy(LlamaSdpaAttention):
         value_states = repeat_kv(value_states, self.num_key_value_groups)
 
         attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+        if self.evicted is not None: # don't regard weights corresponding to keys that have already been evicted
+            for q in range(attn_weights.shape[-2]):
+                attn_weights[:, :, q, :self.evicted.shape[-1]][self.evicted] = torch.finfo(attn_weights.dtype).min
 
         # upcast attention to fp32
         attn_scores = F.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-        attn_scores = F.dropout(attn_weights, p=self.attention_dropout, training=self.training)
+        attn_scores = F.dropout(attn_scores, p=self.attention_dropout, training=self.training)
         
-        sum_scores = attn_scores.sum(axis=-2)
-        num_keys = sum_scores.shape[-1]
+        # add new attention scores to the accumulator 
+        sum_scores_this_pass = attn_scores.sum(axis=-2) # (1, nh, newnk)
+        if self.accum_sum_scores is not None:
+            sum_scores_this_pass[:, :, :self.accum_sum_scores.shape[-1]] += self.accum_sum_scores
+            self.accum_sum_scores = sum_scores_this_pass
+            # update evicted array to be correct size
+            evicted = torch.zeros_like(sum_scores_this_pass, dtype=torch.bool)
+            evicted[:, :, :self.evicted.shape[-1]] = self.evicted
+            self.evicted = evicted
+        else: # nothing is evicted yet
+            self.accum_sum_scores = sum_scores_this_pass
+            self.evicted = torch.zeros_like(self.accum_sum_scores, dtype=torch.bool)
 
-        # create heavy hitter mask
-        num_hh = math.floor(num_keys * self.heavy_hitters_prop)
-        offset = torch.finfo(attn_weights.dtype).min
-        sum_scores = torch.sum(attn_scores[:, :, :num_hh, :], dim=-2) #(nh, nk)
-        sum_scores[:, :, num_hh:] = 0
+        num_tokens = sum_scores_this_pass.shape[-1]
+        num_hh = math.floor(num_tokens * self.heavy_hitters_prop)
+        num_recent = math.floor(num_tokens * self.recent_prop)
+        num_keep = num_hh + num_recent
+        if self.minimum_tokens is not None:
+            num_keep = max(self.minimum_tokens, num_keep)
 
-        is_heavy = torch.zeros_like(attn_scores, dtype=torch.bool)
-        is_heavy[:, :, :num_hh, :num_hh] = True
-
-        for token_index in range(num_hh, num_keys):
-            tmp_attn_index = attn_scores[:, :, token_index, :]
-            _, tmp_topk_index = sum_scores.topk(k=num_hh-1, dim=-1)
-            heavy_token = torch.zeros_like(tmp_attn_index, dtype=torch.bool)
-            heavy_token = heavy_token.scatter(-1, tmp_topk_index, True) #(head, keys)
-            heavy_token[:, :, token_index] = True
-
-            is_heavy[:, :, token_index, :] = heavy_token
-            print(token_index, tmp_attn_index.shape, sum_scores.shape, is_heavy.shape)
-            sum_scores += tmp_attn_index
-            sum_scores = sum_scores * heavy_token
-
-        # create recency mask
-        num_recent = max(self.minimum_tokens, math.floor(num_keys * self.recent_prop))
-        is_recent = torch.ones_like(attn_scores, dtype=torch.bool)
-        is_recent = torch.triu(is_recent, diagonal=-num_recent)
+        num_to_evict = num_tokens - num_keep - self.num_evicted
+        if num_to_evict > 0:
+            self.accum_sum_scores[self.evicted] = torch.finfo(self.accum_sum_scores.dtype).max # don't evict alr evicted
+            # get bottom k accumulated scores of each head that are not protected by recency
+            _, idxs_to_evict = torch.topk(self.accum_sum_scores[:, :, :num_tokens-num_recent], k=num_to_evict, dim=-1, largest=False)
+            self.evicted = self.evicted.scatter(-1, idxs_to_evict, True) # add new evictions to boolean array
+            self.num_evicted += num_to_evict
         
-        # create overall weights mask
-        keep_weights = torch.logical_or(is_heavy, is_recent)
-        keep_weights = torch.tril(keep_weights, diagonal=0)
-        
-        # make the weights we don't want to keep small before we softmax
-        attn_weights[~keep_weights] = torch.finfo(attn_weights.dtype).min
-        attn_scores = F.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-
         attn_output = torch.matmul(attn_scores, value_states)
 
         if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
@@ -261,11 +254,11 @@ class LlamaAttentionHHGreedy(LlamaSdpaAttention):
             attn_output = self.o_proj(attn_output)
 
         if not output_attentions:
-            attn_weights = None
+            attn_scores = None
         
         passes += 1
 
-        return attn_output, attn_weights, past_key_value
+        return attn_output, attn_scores, past_key_value
 
 
 if __name__ == "__main__":
@@ -286,9 +279,15 @@ if __name__ == "__main__":
     # replace the attention layers
     layers = model._modules['model']._modules['layers']._modules
     num_layers = len(layers)
-    for i in range(num_layers // 2, num_layers): 
+    start_replacement = 16
+    heavy_hitters_prop = 0.1
+    recent_prop = 0.05
+    minimum_tokens = 10
+    for i in range(start_replacement, num_layers): 
         attn_layer = layers[f"{i}"]._modules['self_attn']
-        layers[f"{i}"]._modules['self_attn'] = LlamaAttentionHHOracle(attn_layer).to(DEVICE)
+        layers[f"{i}"]._modules['self_attn'] = \
+            LlamaAttentionHHGreedy(attn_layer, heavy_hitters_prop=heavy_hitters_prop, 
+                recent_prop=recent_prop, minimum_tokens=minimum_tokens).to(DEVICE)
         print(f"replaced layer {i}")
 
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(DEVICE)
@@ -296,6 +295,6 @@ if __name__ == "__main__":
     # reload the attention weights
     model.load_state_dict(checkpoint)
 
-    outputs = model.generate(input_ids, do_sample=False, max_length=20000)
+    outputs = model.generate(input_ids, do_sample=False, max_length=4000)
     print(tokenizer.batch_decode(outputs, skip_special_tokens=True)[0])
 
